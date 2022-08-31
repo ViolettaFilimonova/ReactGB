7c16d7656dd31ab20fefaeb7100ab1b18f8af432
ViolettaFilimonova <84204983+ViolettaFilimonova@users.noreply.github.com>
2022-08-31 17:43:49 +0300

    v-for

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css';
 
 import {Form as ClassForm} from "./components-class/Form";
+import {Count as CountClass} from "./components-class/Count";
 import {Count} from "./components/Count";
 import {ChildComponent} from "./components/ChildComponent";
 import {useState} from "react";
@@ -9,19 +10,26 @@ import {useState} from "react";
 function App() {
     const [name, setName] = useState('geek')
     const [count, setCount] = useState(0)
+    const arr = ['Alex', 'Dima', 'Ivan']
     const handleChangeName = (ev) => {
       setName(ev.target.value)
     }
   return (
     <div className="App">
-        <Count name={'Violetta'}/>
+        <h2>Class components</h2>
+        <CountClass/>
         <ClassForm/>
         <hr/>
+        <h2>Function components</h2>
+        <Count name={'Violetta'}/>
         <h3>Parent Component</h3>
         <p>{count}</p>
         <input type="text" onChange={handleChangeName}/>
         <h3>Child Component</h3>
         <ChildComponent handleChangeCount={setCount} name={name}/>
+        {arr.map((item, ind) => {
+            return <p key={ind}>{item}</p>
+        })}
     </div>
   );
 }
--- src/components-class/Count.jsx
+++ src/components-class/Count.jsx
@@ -3,6 +3,7 @@ import React from "react";
 export class Count extends React.Component {
     state = {
         count: 1,
+        arr: ['Ivanov', 'Petrov', 'Sidarov']
     }
     handleClick = () => {
         this.setState({count: this.state.count + 1})
@@ -10,6 +11,9 @@ export class Count extends React.Component {
     render() {
         return(
             <div>
+                {this.state.arr.map((item, ind) => {
+                    return <p key={ind}>{item}</p>
+                })}
                 <p>Count state: {this.state.count}</p>
                 <p>Count props: {this.props.count}</p>
                 <button type="button" onClick={this.handleClick}>click</button>
